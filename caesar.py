672c4b4ec848615b8cb389b3ee9c79c97bce8c3c
Brakau-TD <torsten.drever@outlook.de>
2023-04-13 18:27:17 +0200

    caesar komplett und funktionierend

--- caesar.py
+++ caesar.py
@@ -10,6 +10,12 @@ class Caesar:
     def set_clear_text(self, clear_text: str):
         self._clear_text = clear_text
     
+    def set_decrypted_text(self, decrypted_text: str):
+        self._decrypted_text = decrypted_text
+        
+    def set_encrypted_text(self, encrypted_text: str):
+        self._encrypted_text = encrypted_text
+    
     def give_encrypted_text(self) -> str:
         return self._encrypted_text
     
@@ -23,21 +29,28 @@ class Caesar:
         self._encrypted_text = ""
         for letter in self._clear_text:
             encrypted_letter = ord(letter) + self._encryption_start
-            encrypted_letter = 48 + (encrypted_letter - 122) if encrypted_letter > 125 else encrypted_letter
+            encrypted_letter = 32 + (encrypted_letter - 122) if encrypted_letter > 125 else encrypted_letter
             self._encrypted_text = self._encrypted_text + chr(encrypted_letter)
     
     def decrypt(self, decryption_key: int):
         """returns the string of the decrypted text"""
         for letter in self._encrypted_text:
             decrypted_letter = ord(letter) - decryption_key
-            if decrypted_letter <48:
+            if decrypted_letter <32:
                 decrypted_letter = 122 - abs(decrypted_letter -48)
             self._decrypted_text += chr(decrypted_letter)
 
-caesar = Caesar()
-caesar.set_clear_text("0Helloz")
-caesar.set_encryption_start(3)
-caesar.encrypt()
-print(caesar.give_encrypted_text())
-caesar.decrypt(3)
-print(caesar.give_decrypted_text())
+def caesar_testanwendung():
+    caesar = Caesar()
+    text = input("Enter text to encrypt: ")
+    caesar.set_clear_text(text)
+    caesar.set_encryption_start(3)
+    caesar.encrypt()
+    print(caesar.give_encrypted_text())
+    text2 = input("Give encrypted text to decrypt: ")
+    caesar.set_encrypted_text(text2)
+    caesar.decrypt(3)
+    print(caesar.give_decrypted_text())
+
+if __name__ == "__main__":
+    caesar_testanwendung()
