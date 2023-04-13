3bd2d68add25d30aeee2fc93fcdb7555bdea040d
Brakau-TD <torsten.drever@outlook.de>
2023-04-13 19:23:54 +0200

    test

--- transpositioncipher.py
+++ transpositioncipher.py
@@ -2,5 +2,32 @@
 
 from encryption import Encryption
 
+
 class TranspositionCipher(Encryption):
-    def __init__(self)
+    def __init__(self):
+        self._clear_text = None
+        self._encrypted_text = None
+        self._decrypted_text = ""
+        self._caesar_cipher = 0
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+    def set_clear_text(self, clear_text: str):
+        pass
+
+    def set_decrypted_text(self, decrypted_text: str):
+        pass
+
+    def set_cipher(self, cipher: any):
+        pass
+
+    def give_encrypted_text(self) -> str:
+        pass
+
+    def give_decrypted_text(self) -> str:
+        pass
